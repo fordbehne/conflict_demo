1de67b8d265044e08c6d94ab02fd14836d3c3414
Ford Behne <fordbehne@gmail.com>
2024-05-03 14:11:23 -0400

    merge

--- helloworld.py
+++ helloworld.py
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
 def hello_world(city, state):
     print(f"Hello World {city}, {state}!")
 
 
 def main():
     hello_world("NYC", "New York")
-=======
-def hello_world(city):
-    print(f"Hello World! {city}!")
-
-
-def main():
-    hello_world("NYC")
->>>>>>> cd85a677ff079914bfb71bb39354ee3640671a78
 
 
 main()
